137dc7a5738c9acb4e8cbef52acbcb92c4502eb6
a7u7a <esteban@esrs.co>
2022-05-27 21:31:15 -0400

    testing cms conf

--- lib/posts.ts
+++ lib/posts.ts
@@ -4,7 +4,7 @@ import matter from 'gray-matter'
 import { remark } from 'remark'
 import html from 'remark-html'
 
-const postsDirectory = path.join(process.cwd(), 'content/about')
+const postsDirectory = path.join(process.cwd(), 'content/feed')
 
 // Used to render post index
 export function getSortedPostsData() {
