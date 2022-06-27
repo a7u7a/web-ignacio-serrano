6a0d2c8637545f0b6fd40c844451e7fa619764e9
a7u7a <esteban@esrs.co>
2022-06-27 17:58:55 -0400

    build fix

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -21,7 +21,7 @@ export default function Post({
       <article>
         <h1>{postData.title}</h1>
         <div>
-          <Date dateString={postData.date} />
+          <Date dateString={postData.date} className="" />
         </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
