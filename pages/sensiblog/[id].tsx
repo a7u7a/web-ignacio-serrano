27d6044011a02b4a33ad44dbd12270192de70545
a7u7a <esteban@esrs.co>
2022-07-06 20:13:30 -0400

    base layout sensiblog post

--- pages/sensiblog/[id].tsx
+++ pages/sensiblog/[id].tsx
@@ -4,9 +4,13 @@ import { getAllPostIds, getSensiblogPost } from "../../lib/posts";
 import { sensiblogPost } from "../../interfaces/posts";
 import ReactMarkdown from "react-markdown";
 import FromMarkdownToSensiblog from "../../components/sensiblog/from-markdown";
-
+import SideGradient from "../../components/side-gradient";
+import Image from "next/image";
+import Link from "next/link";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
+import LanguageButton from "../../components/languageBtn";
+import DateEl from "../../components/date";
 
 interface SensiblogPostProps {
   post: sensiblogPost;
@@ -29,9 +33,47 @@ export default function Post({ post }: SensiblogPostProps) {
   }
 
   return (
-    <div className="flex justify-between flex-row h-full bg-black">
+    <div className="flex justify-between flex-row h-screen bg-black overflow-x-auto">
+       <div className="pl-12 pt-12 pb-12">
+        <SideGradient />
+        <div className="flex flex-col">
+          {/* Header */}
+          <div className="flex flex-row justify-between w-96">
+            <Link href={"/sensiblog"}>
+              <div className="h-20 w-48 relative cursor-pointer">
+                <Image
+                  objectFit="contain"
+                  src={"/images/sensiblog.png"}
+                  layout="fill"
+                />
+              </div>
+            </Link>
+            <LanguageButton onClick={toggleLang} lang={lang} color="white" />
+          </div>
+
+          <div className="text-white">
+            {/* Title */}
+            <div className="mt-12 text-5xl font-serif text-white">{title}</div>
+
+            {/* Date */}
+            <div className="text-sm mt-8">
+              <DateEl dateString={post.date} />
+            </div>
+
+            {/* Tags */}
+            <div className="mt-4">
+              {post.tags.map((tag) => (
+                <p key={tag} className="inline mr-2">{`#${tag}`}</p>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
       <div>
-        <FromMarkdownToSensiblog contentMd={content} className="flex flex-row p-12 space-x-12 overflow-x-auto" />
+        <FromMarkdownToSensiblog
+          contentMd={content}
+          className="flex flex-row w-full space-x-12 p-12 "
+        />
       </div>
     </div>
   );
