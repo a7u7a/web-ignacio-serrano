aa49d36c20e64afac677e6bd2f5146ad47043527
a7u7a <esteban@esrs.co>
2022-07-09 19:54:52 -0400

    style fixes

--- pages/posible/[id].tsx
+++ pages/posible/[id].tsx
@@ -18,9 +18,9 @@ interface PosiblePostProps {
 }
 
 export default function Post({ post, modalContent }: PosiblePostProps) {
-  console.log("modalContent", modalContent);
   const [visibleModal, setModalVisibility] = useState(false);
   const [lang, setLang] = useState("spa");
+  console.log("lang", lang);
   const content =
     lang === "spa" ? post.contentSpanish : post.descriptionEnglish;
   var peso = new Intl.NumberFormat("es-CL", {
@@ -44,34 +44,39 @@ export default function Post({ post, modalContent }: PosiblePostProps) {
         visible={visibleModal}
         toggleVisible={setModalVisibility}
       />
-      <div className="absolute top-0 right-0 p-6">
+      <div className="absolute top-0 right-0 p-6 z-10">
         <LanguageButton onClick={toggleLang} lang={lang} color="black" />
       </div>
       <div className="absolute p-6">
         <div className="relative flex flex-col w-80">
-          <div className="flex flex-col pt-24 pl-8">
+          <div className="flex flex-col pt-10 md:pt-24 pl-0 md:pl-8">
             {/* Title */}
             <div className="mt-3 font-light text-3xl w-52">{post.title}</div>
             <div className="flex flex-row space-x-10 mt-6">
               <p className="text-lg">{peso.format(post.price)}</p>
               <button
+                disabled={post.stock > 0 ? false : true}
                 onClick={() => setModalVisibility(true)}
-                className="text-black rounded bg-verde py-0.5 px-1.5 text-sm font-medium"
+                className={`text-black rounded ${
+                  post.stock > 0
+                    ? "bg-verde text-black"
+                    : "bg-gray-200 text-gray-600"
+                } py-0.5 px-1.5 text-sm font-medium`}
               >
-                Comprar
+                {post.stock > 0 ? "Comprar" : "Agotado"}
               </button>
             </div>
           </div>
         </div>
       </div>
-      <div className="pl-14 pt-80">
+      <div className="pl-6 md:pl-14 pt-56 md:pt-80">
         <FromMarkdown contentMd={content} proseClass={"posible-description"} />
       </div>
 
-      <div className="pl-16">
+      <div className="pl-0 md:pl-12">
         <FromMarkdownHorizontal
           contentMd={post.photos}
-          className="flex flex-col md:flex-row w-full pt-2 px-6 sm:px-8 md:px-10 lg:px-10 md:pt-12 pb-20 md:pb-12"
+          className="flex flex-col md:flex-row w-full pt-2 px-6 sm:px-8 md:px-10 lg:px-10 md:pt-12 pb-12"
           proseClass="posible"
         />
       </div>
